22434a7553e0bd89f6010cc99e9ec185b296a498
Shelby Stowe <sstowe@mines.edu>
2026-02-04 11:23:52 -0700

    Create run_HCL_Model.m

---
+++ run_HCL_Model.m
@@ -0,0 +1,15 @@
+% Simulating the HCL Model 
+% as published in Skeldon et al., PLoS Comp Bio (2023)
+
+% Shelby Stowe
+% sstowe@mines.edu
+
+% This is the main code file to run the model 
+% It calls the following functions: 
+
+%% Set Up 
+
+clear
+close all
+clc
+
