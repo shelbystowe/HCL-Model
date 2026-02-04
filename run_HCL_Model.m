f94ca381e35fc3c43cc5355ae9220d3204715d39
Shelby Stowe <sstowe@mines.edu>
2026-02-04 11:50:35 -0700

    Adding set up pieces of code

--- run_HCL_Model.m
+++ run_HCL_Model.m
@@ -13,3 +13,19 @@ clear
 close all
 clc
 
+% Set simulation duration
+n = 1; % days
+t = [0 24*n]; % hours
+
+% Set initial conditions
+H_init = 1; % Homeostatic drive
+x_init = 1; % Circadian drive 
+y_init = 1; % Auxilary variable 
+n_init = 1; % Proportion of activated photoreceptors 
+
+
+
+
+
+
+%% 
