833327c575c2d4c23a3a1b9197600df13de4fbea
Shelby Stowe <sstowe@mines.edu>
2026-02-25 11:21:35 -0700

    Changed three parameter values
    Based on values in Forger Errata 1999

--- Set_Parameters.m
+++ Set_Parameters.m
@@ -1,17 +1,17 @@
 function P = Set_Parameters()
 
 % SET INITIAL CONDITIONS
-P.x_init = -0.2140; % Circadian drive 
-P.y_init = 2.0192; % Auxilary variable 
-P.n_init = 0.6758; % Proportion of activated photoreceptors 
+P.x_init = -0.9642; % Circadian drive 
+P.y_init = -0.4837; % Auxilary variable 
+P.n_init = 0.4128; % Proportion of activated photoreceptors 
 
 % SET PARAMETER VALUES
 % Circadian oscillator parameters
 %P.alpha0  = 0.16/60; % modulates how light affects the fraction of activated photoreceptors
-P.alpha0  = 0.16*60;
+P.alpha0  = 0.05*60; %0.16*60; % CHANGED based on Errata
 P.I0      = 9500; % scaling factor
-P.p       = 0.6; % modulates the steepness of the dose response curve to light
-P.G       = 19.9; % modulates the strength of the effect of light
+P.p       = 0.5; %0.6; % modulates the steepness of the dose response curve to light % CHANGED based on Errata
+P.G       = 33.75/60; %19.9; % modulates the strength of the effect of light % CHANGED based on Errata
 P.b       = 0.4; % modulates timing of maximum light sensitivity 
 P.gamma   = 0.23; % stiffness parameter
 %P.kappa   = (12/pi)*60*60; % time scaling factor
@@ -19,7 +19,7 @@ P.kappa   = (12/pi);
 P.f       = 0.99669; % correction factor 
 P.k       = 0.55; % modulates shape of velocity response curve
 %P.beta    = 0.013/60; % decay rate of photoreceptor activation
-P.beta    = 0.013*60;
+P.beta    = 0.0075*60; %0.013*60; % CHANGED based on Errata
 P.tau_c   = 24.2; % Circadian period
 
 % Circadian wake propensity rhythm parameters 
